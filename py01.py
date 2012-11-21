b05f5f3bf22dafd371baef5c162f8a48a51a4637
Zhang Donglai <donglai.zhang@adelaide.edu.au>
2012-11-21 22:14:20 +1030

    add py01.py file

---
+++ py01.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+  i = 1
+  total=0
+  while i <= 3:
+    total = total + i
+    i = i + 1
+  
+  print "The total = ", total
+  if True:
+    print "total good. "
+    
+if  __name__=='__main__':
+  main()
+  
